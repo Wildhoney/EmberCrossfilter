db517dca64500a6cb0d024f3766994fb900021ac
Wildhoney <adam@zephyrhealthinc.com>
2013-06-06 12:23:43 +0400

    Using Ember.get/Ember.set instead. Fixes #19

--- tests/spec.js
+++ tests/spec.js
@@ -6,7 +6,7 @@ describe('Ember Crossfilter', function() {
         var ArrayController = Ember.ArrayController.extend(EmberCrossfilter, {
 
             init: function() {
-                this.set('content', [
+                Ember.set(this, 'content', [
                     { id: 1, name: 'Cecil', age: 4, colours: ['black', 'white', 'beige'], cuteness: 11 },
                     { id: 2, name: 'Boris', age: 9, colours: ['black', 'white'], cuteness: 5 },
                     { id: 3, name: 'Irina', age: 6, colours: ['ginger'], cuteness: 6 },
@@ -38,11 +38,11 @@ describe('Ember Crossfilter', function() {
     describe('Generic', function() {
 
         it('Can set the content array on the controller.', function() {
-            expect(controller.get('content.length')).toEqual(4);
+            expect(Ember.get(controller, 'content.length')).toEqual(4);
         });
 
         it('Can find the `filterMap` defined on the controller.', function() {
-            expect(controller.get('filterMap')).toBeDefined();
+            expect(Ember.get(controller, 'filterMap')).toBeDefined();
         });
 
     });
@@ -51,19 +51,19 @@ describe('Ember Crossfilter', function() {
 
         it('Can apply default dimension.', function() {
             controller._applyContentChanges();
-            expect(controller.get('content.length')).toEqual(4);
+            expect(Ember.get(controller, 'content.length')).toEqual(4);
         });
 
         it('Can apply a specified dimension.', function() {
             var map = controller.filterMap.name;
             map.value = 'Boris';
             controller._updateContent(map);
-            expect(controller.get('content.length')).toEqual(1);
+            expect(Ember.get(controller, 'content.length')).toEqual(1);
         });
 
         it('Can sort content ascending by name.', function() {
-            controller._sortedContent(controller.get('content'), 'name', true);
-            expect(controller.get('content.firstObject.name')).toEqual('Boris');
+            controller._sortedContent(Ember.get(controller, 'content'), 'name', true);
+            expect(Ember.get(controller, 'content.firstObject.name')).toEqual('Boris');
         });
 
     });
@@ -71,16 +71,16 @@ describe('Ember Crossfilter', function() {
     describe('Crossfilter', function() {
 
         it('Can define the necessary dimensions.', function() {
-            expect(controller.get('_dimensionId')).toBeDefined();
-            expect(controller.get('_dimensionColour')).toBeDefined();
-            expect(controller.get('_dimensionAge')).toBeDefined();
-            expect(controller.get('_dimensionName')).toBeDefined();
-            expect(controller.get('_dimensionCuteness')).toBeDefined();
+            expect(Ember.get(controller, '_dimensionId')).toBeDefined();
+            expect(Ember.get(controller, '_dimensionColour')).toBeDefined();
+            expect(Ember.get(controller, '_dimensionAge')).toBeDefined();
+            expect(Ember.get(controller, '_dimensionName')).toBeDefined();
+            expect(Ember.get(controller, '_dimensionCuteness')).toBeDefined();
         });
 
         it('Can create a valid Crossfilter.', function() {
-            expect(controller.get('_crossfilter')).toBeDefined();
-            expect(controller.get('_crossfilter') instanceof Object).toBeTruthy();
+            expect(Ember.get(controller, '_crossfilter')).toBeDefined();
+            expect(Ember.get(controller, '_crossfilter') instanceof Object).toBeTruthy();
         });
 
     });
@@ -89,12 +89,12 @@ describe('Ember Crossfilter', function() {
 
         it('Can determine that the minAge filter is active.', function() {
             controller.addFilter('minAge', 4);
-            expect(controller.get('activeFilters')).toEqual(['minAge']);
+            expect(Ember.get(controller, 'activeFilters')).toEqual(['minAge']);
         });
 
         it('Can determine that the name filter is not active.', function() {
             controller.addFilter('colour', 'black');
-            expect(controller.get('activeFilters')).toEqual(['colour']);
+            expect(Ember.get(controller, 'activeFilters')).toEqual(['colour']);
         });
 
         it('Can determine that the name filter is active via helper method.', function() {
@@ -110,7 +110,7 @@ describe('Ember Crossfilter', function() {
         it('Can determine that the many filters are active.', function() {
             controller.addFilter('name', 'Cecil');
             controller.addFilter('minAge', 7);
-            expect(controller.get('activeFilters')).toEqual(['name', 'minAge']);
+            expect(Ember.get(controller, 'activeFilters')).toEqual(['name', 'minAge']);
         });
 
     });
@@ -119,48 +119,48 @@ describe('Ember Crossfilter', function() {
 
         it('Can filter the content for filterExact dimension.', function() {
             controller.addFilter('name', 'Boris');
-            expect(controller.get('content.length')).toEqual(1);
+            expect(Ember.get(controller, 'content.length')).toEqual(1);
         });
 
         it('Can filter the content for filterInArray dimension.', function() {
             controller.addFilter('colour', 'black');
-            expect(controller.get('content.length')).toEqual(3);
+            expect(Ember.get(controller, 'content.length')).toEqual(3);
         });
 
         it('Can filter the content for filterFunction dimension.', function() {
             controller.addFilter('isCute');
-            expect(controller.get('content.length')).toEqual(1);
+            expect(Ember.get(controller, 'content.length')).toEqual(1);
         });
 
         it('Can filter the content for filterRangeMin dimension.', function() {
             controller.addFilter('minAge', 5);
-            expect(controller.get('content.length')).toEqual(3);
+            expect(Ember.get(controller, 'content.length')).toEqual(3);
         });
 
         it('Can filter the content for filterRangeMax dimension.', function() {
             controller.addFilter('maxAge', 8);
-            expect(controller.get('content.length')).toEqual(2);
+            expect(Ember.get(controller, 'content.length')).toEqual(2);
         });
 
         it('Can clear the name filter.', function() {
             controller.addFilter('name', 'Boris');
             controller.addFilter('minAge', 6);
             controller.removeFilter('name');
-            expect(controller.get('content.length')).toEqual(3);
+            expect(Ember.get(controller, 'content.length')).toEqual(3);
         });
 
         it('Can clear the minAge filter.', function() {
             controller.addFilter('name', 'Boris');
             controller.addFilter('minAge', 100);
             controller.removeFilter('minAge');
-            expect(controller.get('content.length')).toEqual(1);
+            expect(Ember.get(controller, 'content.length')).toEqual(1);
         });
 
         it('Can clear all of the filtering.', function() {
             controller.addFilter('name', 'Boris');
             controller.addFilter('maxAge', 8);
             controller.clearAllFilters();
-            expect(controller.get('content.length')).toEqual(4);
+            expect(Ember.get(controller, 'content.length')).toEqual(4);
         });
 
     });
@@ -169,22 +169,22 @@ describe('Ember Crossfilter', function() {
 
         it('Can sort content ascending by name.', function() {
             controller.sortContent('name', true);
-            expect(controller.get('content.firstObject.name')).toEqual('Boris');
+            expect(Ember.get(controller, 'content.firstObject.name')).toEqual('Boris');
         });
 
         it('Can sort content descending by name.', function() {
             controller.sortContent('name', false);
-            expect(controller.get('content.firstObject.name')).toEqual('Jimmy');
+            expect(Ember.get(controller, 'content.firstObject.name')).toEqual('Jimmy');
         });
 
         it('Can sort content ascending by cuteness level.', function() {
             controller.sortContent('cuteness', true);
-            expect(controller.get('content.firstObject.name')).toEqual('Jimmy');
+            expect(Ember.get(controller, 'content.firstObject.name')).toEqual('Jimmy');
         });
 
         it('Can sort content descending by cuteness level.', function() {
             controller.sortContent('cuteness', false);
-            expect(controller.get('content.firstObject.name')).toEqual('Cecil');
+            expect(Ember.get(controller, 'content.firstObject.name')).toEqual('Cecil');
         });
 
     });
